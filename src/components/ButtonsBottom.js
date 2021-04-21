f14206f321d52da748d08b85e3e89ce02dde4ed7
Guillaume Gruber <lagrube1@gmail.com>
2021-04-21 19:47:56 +0200

    modif taille github linkedin contact

--- src/components/ButtonsBottom.js
+++ src/components/ButtonsBottom.js
@@ -10,7 +10,7 @@ const ButtonsBottom = (props) => {
             <span>&#10092;</span>
           </NavLink>
         )}
-        <p className="center">Switch</p>
+        <p className="center">Swipe</p>
         {props.right && (
           <NavLink to={props.right} className="right hover">
             <span>&#10093;</span>
